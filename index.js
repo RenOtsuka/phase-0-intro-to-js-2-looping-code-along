d33a4e6a4ca2462aa2c42ae0d4e00936d8fd5789
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2021-12-28 10:43:30 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(cards, event){
+
+    const cardarr = []
+    for(let  i = 0; i < cards.length; i++){
+       cardarr[i] = `Thank you, ${cards[i]}, for the wonderful ${event} gift!`;
+    }
+
+    return cardarr;
+}
+
+function countDown(n){
+    let i = n;
+    while(i >= 0){
+        console.log(i);
+        i--;
+    }
+}
+
+
